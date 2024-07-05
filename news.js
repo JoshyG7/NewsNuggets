b8cabb69e176280f0e100d383816b2b790a65edd
JoshiG7 <127643882+JoshyG7@users.noreply.github.com>
2024-07-05 22:45:41 +0530

    Update news.js

--- news.js
+++ news.js
@@ -6,20 +6,30 @@
     let heading=document.querySelector(".heading");
     let result=document.querySelector(".result");
     let watherImage=document.querySelector(".watherImage");
-
-
+    let information=document.querySelector(".information");
+    let AdditionalFeatures=document.querySelector(".AdditionalFeatures");
+    let AboutandContact=document.querySelector(".AboutandContact");
+    let Categories=document.querySelector(".Categories");
 
     let data;
-    let i=0,j=8,d;
-    let infoName="india";
+    let i=0,j=6,d;
+    let infoName="naruto";
     let title;
-    heading.style.visibility = "hidden";
     result.style.visibility = "hidden";
     info.style.visibility = "hidden";
     moreInfo.style.visibility = "hidden";
     function showData(){
         if(j<=d){
-            if(i<8){
+            if(i<6){
+                while(i < j && i < data.articles.length){
+                    info.innerHTML+=`<div class="box">
+                    <img src="${data.articles[i].image}">
+                    <h1>${data.articles[i].title}</h1><br>
+                    <p style="color: rgb(177, 94, 209);">Source: ${data.articles[i].source.name}</p>
+                    </div>`;    
+                    i++;
+                }
+    }else if(i>=6){
         while(i < j && i < data.articles.length){
             info.innerHTML+=`<div class="box">
             <img src="${data.articles[i].image}">
@@ -28,30 +38,22 @@
             </div>`;    
             i++;
         }
-        j+=2;
-    }else if(i==8){
-        while(i < j && i < data.articles.length){
-            info.innerHTML+=`<div class="box">
-            <img src="${data.articles[i].image}">
-            <h1>${data.articles[i].title}</h1><br>
-            <p style="color: rgb(177, 94, 209);">Source: ${data.articles[i].source.name}</p>
-            </div>`;    
-            i++;
-        }
+        j=data.data.length;
     }
+    j+=2;
+    console.log(j);
         getclick();
-    }else{
+    }
+    else{
         alert("There is no Further Information.");
     }
     watherImage.style.visibility = "hidden";
-    heading.style.visibility = "visible";
     result.style.visibility = "visible";
     info.style.visibility = "visible";
     moreInfo.style.visibility = "visible";
     }
     const getInto=async()=>{
     watherImage.style.visibility = "visible";
-    heading.style.visibility = "hidden";
     result.style.visibility = "hidden";
     info.style.visibility = "hidden";
     moreInfo.style.visibility = "hidden";
@@ -59,7 +61,7 @@
         let respone= await fetch(searchUrl);
         data=await respone.json();
         d=data.articles.length;
-        resultInfo.innerText=data.articles.length; 
+        resultInfo.innerText=data.articles.length;
         showData();
     }
     getInto();
@@ -72,16 +74,23 @@
     let boxes=document.querySelectorAll(".box");
     boxes.forEach((box,index)=>{
         box.addEventListener("click",()=>{
-
-            localStorage.setItem("publishedAt",data.articles[index].publishedAt);
-            localStorage.setItem("source",data.articles[index].source.name);
-            localStorage.setItem("author",data.articles[index].author);
-            localStorage.setItem('sharedText', data.articles[index].title);
-            localStorage.setItem("description",data.articles[index].description);
-            localStorage.setItem("content",data.articles[index].content);
-            localStorage.setItem("information",data.articles[index].image);
-                localStorage.setItem("url",data.articles[index].url);
-                window.location.href = 'newsInfo.html';
+            Categories.style.visibility = "hidden";
+            AboutandContact.style.visibility = "hidden";
+            AdditionalFeatures.style.visibility = "hidden";
+            result.style.visibility = "hidden";
+            info.style.visibility = "hidden";
+            moreInfo.style.visibility = "hidden";
+            heading.style.visibility="hidden";
+            info.style.position="fixed";
+            information.style.visibility="visible";
+            document.querySelector(".publishedAt span").innerText=data.articles[index].publishedAt;
+            document.querySelector(".source span").innerText=data.articles[index].source.name;
+            document.querySelector(".author span").innerText=data.articles[index].author;
+            document.querySelector(".title").innerText = data.articles[index].title;
+            document.querySelector(".description").innerText=data.articles[index].description;
+            document.querySelector(".content span").innerText=data.articles[index].content;
+            document.querySelector(".information img").src=data.articles[index].image;
+            document.querySelector(".url").href=data.articles[index].url;
         });
     });
 }
@@ -103,4 +112,93 @@ headingSearch.addEventListener("keyup",(e)=>{
         j = 8; // Reset the batch size for new search results
         getInto();
     }
+})
+
+information.querySelector(".remove").addEventListener("click",()=>{
+    Categories.style.visibility = "visible";
+    AboutandContact.style.visibility = "visible";
+    AdditionalFeatures.style.visibility = "visible";
+    result.style.visibility = "visible";
+    info.style.visibility = "visible";
+    moreInfo.style.visibility = "visible";
+    heading.style.visibility="visible";
+    info.style.position="static";
+    information.style.visibility="hidden";
+});
+
+let item=document.querySelectorAll(".item");
+item.forEach((itemInfo,index)=>{
+    itemInfo.addEventListener("click",()=>{
+        infoName=itemInfo.innerText;
+        info.innerHTML = '';
+        i=0,j=8;
+        getInto();
+    })
+})
+
+function forAbout(){
+    AboutandContact.querySelector(".AboutandContact ul").classList.toggle("showing");
+}
+function forAdditional(){
+    AdditionalFeatures.querySelector(".AdditionalFeatures ul").classList.toggle("showing");
+}
+AdditionalFeatures.querySelector(".Additional").addEventListener("click",()=>{
+    AdditionalFeatures.querySelector(".AdditionalFeatures ul").classList.toggle("showing");
+})
+
+AboutandContact.querySelector(".about").addEventListener("click",()=>{
+    AboutandContact.querySelector(".AboutandContact ul").classList.toggle("showing");
+})
+
+let item1=document.querySelectorAll(".item1");
+    item1.forEach((itemInfo,index)=>{
+        itemInfo.addEventListener("click",()=>{
+            infoName=itemInfo.innerText;
+            info.innerHTML = '';
+            i=0,j=8;
+            forAdditional();
+            getInto();
+        })
+    })
+
+    function blockInfo(){
+        Categories.style.visibility = "hidden";
+        AboutandContact.style.visibility = "hidden";
+        AdditionalFeatures.style.visibility = "hidden";
+        result.style.visibility = "hidden";
+        info.style.visibility = "hidden";
+        moreInfo.style.visibility = "hidden";
+        heading.style.visibility="hidden";
+        info.style.position="fixed";
+    }
+    function NotBlock(){
+        Categories.style.visibility = "visible";
+        AboutandContact.style.visibility = "visible";
+        AdditionalFeatures.style.visibility = "visible";
+        result.style.visibility = "visible";
+        info.style.visibility = "visible";
+        moreInfo.style.visibility = "visible";
+        heading.style.visibility="visible";
+        info.style.position="static";
+    }
+    let item2=document.querySelectorAll(".item2");
+    item2.forEach((itemInfo,index)=>{
+    itemInfo.addEventListener("click",()=>{
+        let nameInformation=itemInfo.innerText;
+        if(nameInformation=='About Us'){
+            document.querySelector(".aboutUS").style.visibility="visible";
+        }else if(nameInformation=='Contact'){
+            document.querySelector(".contactUs").style.visibility="visible";
+        }
+        blockInfo();
+        forAbout();
+    })
+})
+document.querySelector(".removeFromAbourt").addEventListener("click",()=>{
+    document.querySelector(".aboutUS").style.visibility="hidden";
+    NotBlock();
+})
+document.querySelector(".removeFromContact").addEventListener("click",()=>{
+    document.querySelector(".contactUs").style.visibility="hidden";
+    NotBlock();
 })
